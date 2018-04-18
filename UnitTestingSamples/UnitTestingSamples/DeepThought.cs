dc4fb0993f869f68a00782f3713ca8115a75214d
mrhao <mrhao@jiawel>
2018-04-18 18:15:54 +0800

    Chapter19 Test

---
+++ UnitTestingSamples/UnitTestingSamples/DeepThought.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingSamples
+{
+   public class DeepThought
+    {
+        public int TheAnswerOfTheUltimateQuestionOfLifeTheUniverseAndEverything() => 41;
+
+        //public int TheAnswerOfTheUltimateQuestionOfLifeTheUniverseAndEverything()
+        //{
+        //    return 42;
+        //}
+    }
+}
---
+++ UnitTestingSamples/UnitTestingSamplesTests/DeepThoughtTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestingSamples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingSamples.Tests
+{
+    [TestClass()]
+    public class DeepThoughtTests
+    {
+        [TestMethod()]
+        public void TheAnswerOfTheUltimateQuestionOfLifeTheUniverseAndEverythingTest()
+        {
+            //arrange
+            int expected = 42;
+            var dt = new DeepThought();
+
+            //act
+            int actual = dt.TheAnswerOfTheUltimateQuestionOfLifeTheUniverseAndEverything();
+
+            //assert
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
